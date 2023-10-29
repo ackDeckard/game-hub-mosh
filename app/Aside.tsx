d1ab103190d92e753ee64f5a9ad272d5359aa1db
Pablo <ackdeckard@proton.me>
2023-10-29 19:10:21 -0300

    Basic layout with responsivness

---
+++ app/Aside.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Aside = () => {
+  return <div className="hidden bg-yellow-500 sm:block">Aside</div>;
+};
+
+export default Aside;
---
+++ app/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <nav className="bg-orange-600">NavBar</nav>;
+};
+
+export default NavBar;
