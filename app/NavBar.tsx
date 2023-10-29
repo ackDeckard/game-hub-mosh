74fe5aeb252663d40973dfa145e5058f43e41c74
Pablo <ackdeckard@proton.me>
2023-10-29 19:37:54 -0300

    Navbar, added logo

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const NavBar = () => {
-  return <nav className="bg-orange-600">NavBar</nav>;
+  return (
+    <nav className="grid grid-flow-col">
+      <Image
+        src="logo.png"
+        alt="logo"
+        width={0}
+        height={0}
+        unoptimized
+        className="h-14 w-auto"
+      />
+      <div>Any text</div>
+    </nav>
+  );
 };
 
 export default NavBar;
